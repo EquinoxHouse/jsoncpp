b7b107fd1d979aae50b78e319f31d619097d10a8
Akashi <akashic.records.of.the.abyss@gmail.com>
2025-10-25 01:13:10 +0100

    Code cleanup

--- include/json/config.h
+++ include/json/config.h
@@ -6,24 +6,6 @@
 #ifndef JSONCPP_CONFIG_H_INCLUDED
 #define JSONCPP_CONFIG_H_INCLUDED
 
-/// If defined, indicates that json library is embedded in CppTL library.
-// # define JSONCPP_IN_CPPTL 1
-
-/// If defined, indicates that json may leverage CppTL library
-// #  define JSONCPP_USE_CPPTL 1
-/// If defined, indicates that cpptl vector based map should be used instead of std::map
-/// as Value container.
-// #  define JSONCPP_USE_CPPTL_SMALLMAP 1
-/// If defined, indicates that Json specific container should be used
-/// (hash table & simple deque container with customizable allocator).
-/// THIS FEATURE IS STILL EXPERIMENTAL! There is know bugs: See #3177332
-// #  define JSONCPP_VALUE_USE_INTERNAL_MAP 1
-/// Force usage of standard new/malloc based allocator instead of memory pool based allocator.
-/// The memory pools allocator used optimization (initializing Value and ValueInternalLink
-/// as if it was a POD) that may cause some validation tool to report errors.
-/// Only has effects if JSONCPP_VALUE_USE_INTERNAL_MAP is defined.
-// #  define JSONCPP_USE_SIMPLE_INTERNAL_ALLOCATOR 1
-
 /// If defined, indicates that Json use exception to report invalid type manipulation
 /// instead of C assert macro.
 #define JSONCPP_USE_EXCEPTION 1
